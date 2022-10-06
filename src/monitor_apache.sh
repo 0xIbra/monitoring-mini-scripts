36035a4b4f92a6decb62fbac4f597e3357b88197
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-10-06 11:44:42 +0200

    More improvements on apache script

--- src/monitor_apache.sh
+++ src/monitor_apache.sh
@@ -8,21 +8,16 @@ if [[ -z "${DOWNTIME_BEFORE_RESTART}" ]]; then
   DOWNTIME_BEFORE_RESTART=900
 fi
 
-function is_running() {
-    servicename=$1
-    
-    servstat=$(service $servicename status)
-    if [[ $servstat == *"active (running)"* ]]; then
-      echo "1"
-    else echo "0"
-    fi
-}
-
 if [[ $(is_running apache2) == "0" ]]; then
     # apache down, verify downtime
 
     now=$(date "+%F %T")
     downdate=$(service apache2 status | grep -oP 'since ([a-zA-Z]+) (.*) ([a-zA-Z]+);' | awk '{print $3,$4;}')
+    if [[ $downdate == "" ]]; then
+      log "apache downtime unavailable, maybe it was stopped purposefully"
+      exit
+    fi
+
 
     now_timestamp=$(date -d "$now" '+%s')
     down_timestamp=$(date -d "$downdate" '+%s')
@@ -37,17 +32,21 @@ if [[ $(is_running apache2) == "0" ]]; then
 
 
     # if downtime > 15 mins, then restart apache
-    if [[ downtime > 900 ]]; then
+    if [[ $downtime > $DOWNTIME_BEFORE_RESTART ]]; then
       log "apache been down for $down_text, attempting to restart..."
       response=$(service apache2 restart &> /dev/null)
-      
+
+      # wait for 10 seconds before proceeding
+      sleep 10
+
+      # check if apache is now up
       if [[ $(is_running apache2) == "1" ]]; then
         # apache is now up
         text="apache restarted successfully"
         log "${text}"
 
         if [[ $(string_contains "https://" "$MONITORING_SLACK_WEBHOOK") == "1" ]]; then
-          slack_notification "$MONITORING_SLACK_WEBHOOK" "*[$(hostname)]$(date_prefix)* $text"
+          slack_notification "$MONITORING_SLACK_WEBHOOK" "[$(hostname)]$(date_prefix) $text"
         fi
 
       else
@@ -57,14 +56,18 @@ if [[ $(is_running apache2) == "0" ]]; then
 
         if [[ $(string_contains "https://" "$MONITORING_SLACK_WEBHOOK") == "1" ]]; then
           # multi-line log with contents of `service apache2 status` as a code block below
-          text="*[$(hostname)]$(date_prefix)* $text\n\napache2 status:\n\`\`\` $(service apache2 status) \`\`\`"
+          text="[$(hostname)]$(date_prefix) $text\n\napache2 status:\n\`\`\` $(service apache2 status) \`\`\`"
 
           slack_notification "$MONITORING_SLACK_WEBHOOK" "$text"
         fi
 
       fi
 
-    fi
+    else
+      # apache been down for less than $DOWNTIME_BEFORE_RESTART (default: 15 mins)
 
+      log "apache has been down only for $down_text, no action taken."
+
+    fi
 
 fi
--- src/utils.sh
+++ src/utils.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# function checks if a given service is running or not
+function is_running() {
+    servicename=$1
+    
+    servstat=$(service $servicename status)
+    if [[ $servstat == *"active (running)"* ]]; then
+      echo "1"
+    else
+      echo "0"
+    fi
+}
+
+# checks if a string contains another given string
 function string_contains() {
     needle=$1
     haystack=$2
@@ -11,16 +24,19 @@ function string_contains() {
     fi
 }
 
+# returns a formatted date string for logging
 function date_prefix() {
     echo "[$(date "+%Y-%m-%d %H:%M:%S")]"
 }
 
+# logs text with date time information
 function log() {
     log_text=$1
 
     echo "$(date_prefix) ${log_text}"
 }
 
+# logs text with date time and host information
 function log_with_host() {
     log_text=$1
 
