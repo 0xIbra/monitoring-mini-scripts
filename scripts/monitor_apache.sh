27e1bcf4de857b94e3cbae899ca57009f5b92a36
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-10-07 22:52:43 +0200

    Apache script improved
    Handling cases where the downtime cannot be retrieved from apache status detrails.
    In this case, the script waits for 10 minutes and attempts to restart apache.
    This way, by waiting 10 mins, we avoid interrupting certbot cert updates or any other process that requires apache be down temporarily.

--- scripts/monitor_apache.sh
+++ scripts/monitor_apache.sh
@@ -8,16 +8,62 @@ if [[ -z "${DOWNTIME_BEFORE_RESTART}" ]]; then
   DOWNTIME_BEFORE_RESTART=900
 fi
 
+function restart_successful_handler() {
+  text="apache restarted successfully"
+  log "${text}"
+
+  if [[ $(string_contains "https://" "$APACHE_MONITOR_SLACK_HOOK") == "1" ]]; then
+    slack_notification "$APACHE_MONITOR_SLACK_HOOK" "[$(hostname)]$(date_prefix) $text"
+  fi
+}
+
+function restart_failed_handler() {
+  text="could not restart apache"
+  log "$text"
+
+  if [[ $(string_contains "https://" "$APACHE_MONITOR_SLACK_HOOK") == "1" ]]; then
+    # multi-line log with contents of `service apache2 status` as a code block below
+    text="[$(hostname)]$(date_prefix) $text\n\napache2 status:\n\`\`\` $(service apache2 status) \`\`\`"
+
+    slack_notification "$APACHE_MONITOR_SLACK_HOOK" "$text"
+  fi
+}
+
 if [[ $(is_running apache2) == "0" ]]; then
     # apache down, verify downtime
 
+    # retrieving downtime from service status
     now=$(date "+%F %T")
     downdate=$(service apache2 status | grep -oP 'since ([a-zA-Z]+) (.*) ([a-zA-Z]+);' | awk '{print $3,$4;}')
+
+    # check if downtime available, if not, then wait 10 minutes and try to restart apache
+    # waiting 10 minutes to avoid, interrupting ssl update certbot process which requires that apache be down
     if [[ $downdate == "" ]]; then
-      log "apache downtime unavailable, maybe it was stopped purposefully"
-      exit
+      log "apache downtime unavailable, waiting 10 minutes before attempting to restart"
+
+      sleep 600
+
+      # check again if apache still down
+      if [[ $(is_running apache2) == "0" ]]; then
+        # apache still down after 10 minutes
+        log "apache is still down after 10 minutes of waiting, attempting to restart..."
+        response=$(service apache2 restart &> /dev/null)
+
+        sleep 10
+
+        if [[ $(is_running apache2) == "1" ]]; then
+          restart_successful_handler
+        else
+          restart_failed_handler
+        fi
+
+      else
+        log "apache up on it's own after waiting a bit, no action to take."
+      fi
+
     fi
 
+    # if here, downtime was retrieved
 
     now_timestamp=$(date -d "$now" '+%s')
     down_timestamp=$(date -d "$downdate" '+%s')
@@ -30,9 +76,8 @@ if [[ $(is_running apache2) == "0" ]]; then
       down_text="${downtime} secs"
     fi
 
-
     # if downtime > 15 mins, then restart apache
-    if [[ $downtime > $DOWNTIME_BEFORE_RESTART ]]; then
+    if [[ $downtime -gt $DOWNTIME_BEFORE_RESTART ]]; then
       log "apache been down for $down_text, attempting to restart..."
       response=$(service apache2 restart &> /dev/null)
 
@@ -42,32 +87,15 @@ if [[ $(is_running apache2) == "0" ]]; then
       # check if apache is now up
       if [[ $(is_running apache2) == "1" ]]; then
         # apache is now up
-        text="apache restarted successfully"
-        log "${text}"
-
-        if [[ $(string_contains "https://" "$MONITORING_SLACK_WEBHOOK") == "1" ]]; then
-          slack_notification "$MONITORING_SLACK_WEBHOOK" "[$(hostname)]$(date_prefix) $text"
-        fi
-
+        restart_successful_handler
       else
         # restart did not work, apache still down
-        text="could not restart apache"
-        log "$text"
-
-        if [[ $(string_contains "https://" "$MONITORING_SLACK_WEBHOOK") == "1" ]]; then
-          # multi-line log with contents of `service apache2 status` as a code block below
-          text="[$(hostname)]$(date_prefix) $text\n\napache2 status:\n\`\`\` $(service apache2 status) \`\`\`"
-
-          slack_notification "$MONITORING_SLACK_WEBHOOK" "$text"
-        fi
-
+        restart_failed_handler
       fi
 
     else
       # apache been down for less than $DOWNTIME_BEFORE_RESTART (default: 15 mins)
-
       log "apache has been down only for $down_text, no action taken."
-
     fi
 
 fi
